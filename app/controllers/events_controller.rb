821cf3c1ed43ae59e62faf70718ab7d85c754194
Hein02 <117172810+Hein02@users.noreply.github.com>
2023-01-14 03:09:17 -0800

    Update EventsController & routes to create new evt
    Add #new and #create actions to EventsController.
    Add Event new, create, and show pages.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,25 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+
+    if @event.save
+      flash[:success] = 'You have successfully created an event.'
+      redirect_to @event
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:location, :date)
+  end
 end
