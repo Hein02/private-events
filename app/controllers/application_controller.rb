0692389e8dfebfcda55b132158930c60b094e2a5
Hein02 <117172810+Hein02@users.noreply.github.com>
2023-01-14 02:27:26 -0800

    Fix undefined method `protect_from_forgery'
    Problem
    - NoMethodError
    - undefined method `protect_from_forgery' for ApplicationRecord:Class
    
    Reason
    - Place the config codes inside application_record instead of
    application_controller
    
    Solution
    - Move the codes to application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  # Configuration for adding Custom Fields to Devise
+  # https://gist.github.com/withoutwax/46a05861aa4750384df971b641170407
+  protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,15 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
-  # Configuration for adding Custom Fields to Devise
-  # https://gist.github.com/withoutwax/46a05861aa4750384df971b641170407
-  protect_from_forgery with: :exception
-
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
-  end
 end
